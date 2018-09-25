b3de4a26be93e4fab2fe8f0232b6e7bb19ed00e2
Nabil Rahman <nabilrahman@u.boisestate.edu>
2018-09-25 01:14:28 -0600

    Adds link to author's name.

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" type="text/css" href="css/footer.css">
+<link rel="stylesheet" type="text/css" href="css/animate.css">
 <div class="footer">
-    <p>THE MAP CHAT &copy; <?php echo date("Y"); ?> Nabil Rahman.</p>
+    <p>THE MAP CHAT &copy; <?php echo date("Y"); ?> &nbsp; |  &nbsp; <a href="http://www.nr-creation.com" class="amber-text hvr-grow">Nabil Rahman</a></p>
 </div>
